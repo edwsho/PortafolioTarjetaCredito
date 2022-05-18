ac203708bff017ecb1d72fed1c28f268eb5a2d65
Edward de Nobrega <enobrega_ext@dataifx.com>
2022-05-18 14:22:32 -0400

    Add Initial Components

--- TarjetaCredito-FrontEnd/CRUDTarjetasCreadito/src/app/app-routing.module.ts
+++ TarjetaCredito-FrontEnd/CRUDTarjetasCreadito/src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreditCardComponent } from './Components/credit-card/credit-card.component';
+import { HomeComponentComponent } from './Components/home-component/home-component.component';
 
-const routes: Routes = [];
+/*
+Anado los componentes al servicio de rutas de Angular dentro de esta constante
+*/
+const routes: Routes = [
 
+
+  { path:'HomePage', component:HomeComponentComponent},
+  { path:'CreditCard', component:CreditCardComponent},
+  // { path:'Tablee', component:PageNotFounfComponent},
+  // { path:'StudentsTable', component:StudentsComponent},
+  // { path:'StaffTable', component:StaffComponent},
+  { path: '**', redirectTo: '/HomePage' }
+
+
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- TarjetaCredito-FrontEnd/CRUDTarjetasCreadito/src/app/app.module.ts
+++ TarjetaCredito-FrontEnd/CRUDTarjetasCreadito/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './Components/side-bar/side-bar.component';
+import { HomeComponentComponent } from './Components/home-component/home-component.component';
+import { CreditCardComponent } from './Components/credit-card/credit-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    HomeComponentComponent,
+    CreditCardComponent
   ],
   imports: [
     BrowserModule,
